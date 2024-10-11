78f11b7eaf0351b64fab4dc19848de1af43b4d50
yashin20 <wlddud8@gmail.com>
2024-10-11 15:41:58 +0900

    #0. 프로젝트 생성 / application.yml 구성

---
+++ src/test/java/project/board_service/BoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.board_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
