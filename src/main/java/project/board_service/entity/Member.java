7b369bf391ef6d62284e85834ec68d973649046c
yashin20 <wlddud8@gmail.com>
2024-10-13 03:03:10 +0900

    #1. SpringSecurity 를 이용한 회원 로그인 구현

--- src/main/java/project/board_service/entity/Member.java
+++ src/main/java/project/board_service/entity/Member.java
@@ -1,9 +1,6 @@
 package project.board_service.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,5 +22,6 @@ public class Member extends BaseEntity {
     private String email;
     private String phone;
 
+    @Enumerated(EnumType.STRING)
     private MemberRole role;
 }
